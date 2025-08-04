7a8e3f46caebd8ab2eae48282c5ea78f5f65bc92
achiya reuven <achiya3144@gmail.com>
2025-08-04 17:55:26 +0300

    I updated main for testing.

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-from utils import load_data
+from utils import load_data,write_to_Jason
 from analysis.data_analyzer import DataAnalyzer
+from cleaning.data_cleaner import DataCleaner
 
 df = load_data(r"C:\Users\achiy\PycharmProjects\Test_project\data\tweets_dataset.csv")
-# print(a.shape)
-a = df.groupby("Biased")["Text"].count
-print(a)
+
+
+
+a =DataAnalyzer(df)
+a.amount_of_tweets()
+a.average_words()
+a.three_longest_tweets()
+a.amount_of_uppercase()
+
+s=(a.return_dict_result())
+
+# write_to_Jason(s,r"C:\Users\achiy\PycharmProjects\Test_project\results\result.json")
+
+
+clen=DataCleaner(df)
+
+clen.update_to_lowercase()
 
 
 
 
 
-# print(df.value_counts("Biased"))
-# count_mising_val = df.isnull().sum()
-# print(count_mising_val)
-# a= (df['Biased'].unique())
-# print(a)
-# for val in a :
-#     if val == 1:
-#         print(df.value_counts("Biased")[0])
-#     elif val == 0 :
-#         print(df.value_counts("Biased")[1])
 
-d = DataAnalyzer(df)
+
+
+
+
+
+
+
+
 
