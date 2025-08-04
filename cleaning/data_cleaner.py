9b1c80bfdb651f2da3036d7b9b67b38d713b8fd8
achiya reuven <achiya3144@gmail.com>
2025-08-04 19:10:43 +0300

    I added a function that remove rows taht is unclassified and u added a function to return the final DF

--- cleaning/data_cleaner.py
+++ cleaning/data_cleaner.py
@@ -5,6 +5,9 @@ class DataCleaner():
         self.df = df
         self.df_resulte = None
 
+    def remove_unclassified_rows(self):
+        self.df = self.df([self.df["Biased"]]==0) | ([self.df["Biased"]]==1)
+
     def update_to_lowercase(self):
         self.df["Text"] =self.df["Text"].astype(str).apply(lambda x: x.lower())
 
@@ -12,10 +15,15 @@ class DataCleaner():
         def clean_data(text):
             punctuation_marks = [".",",""?","!","_"]
             for punctuation in  punctuation_marks:
-                text =str(text.replace(punctuation,"")
+                text =str(text.replace(punctuation,""))
             return text
 
         self.df["Text"] = self.df["Text"].apply(clean_data)
+
+    def get_cleaned_df(self):
+        return self.df[["Text", "Biased"]]
+
+
 
 
 
