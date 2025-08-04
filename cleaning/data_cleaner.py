e24441e6c574441628471556637508e296a1fac6
achiya reuven <achiya3144@gmail.com>
2025-08-04 19:02:36 +0300

    I added remove_punctuation_marks to replace punctuation

--- cleaning/data_cleaner.py
+++ cleaning/data_cleaner.py
@@ -8,6 +8,18 @@ class DataCleaner():
     def update_to_lowercase(self):
         self.df["Text"] =self.df["Text"].astype(str).apply(lambda x: x.lower())
 
+    def remove_punctuation_marks(self):
+        def clean_data(text):
+            punctuation_marks = [".",",""?","!","_"]
+            for punctuation in  punctuation_marks:
+                text =str(text.replace(punctuation,"")
+            return text
+
+        self.df["Text"] = self.df["Text"].apply(clean_data)
+
+
+
+
 
 
 
