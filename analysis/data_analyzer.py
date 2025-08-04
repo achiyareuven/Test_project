0efc62e73dd097091830bd60a2f6828ee548c440
achiya reuven <achiya3144@gmail.com>
2025-08-04 16:39:27 +0300

    add a function that return three_longest_tweets

--- analysis/data_analyzer.py
+++ analysis/data_analyzer.py
@@ -29,22 +29,37 @@ class DataAnalyzer:
 
 
     def average_words(self):
-        self.df["word count"] = self.df[self.df]["Text"].apply(lambda x:len(x.split()))
-        antisemitic  = self.df[self.df["Biased"]]=1["Text"].mean
-        non_antisemitic  = self.df[self.df["Biased"]]=0["Text"].mean
-        total = self.df[self.df["Text"]].mean
+        self.df["word count"] = self.df["Text"].apply(lambda x:len(str(x).split()))
+        antisemitic  = self.df[self.df["Biased"]==1]["word count"].mean()
+        non_antisemitic  = self.df[self.df["Biased"]==0]["word count"].mean()
+        total = self.df[self.df["word count"]].mean()
 
         self.result["average_length"]={
              "antisemitic": antisemitic,
              "non_antisemitic": non_antisemitic,
              "total": total
         }
+    def three_longest_tweets(self):
+        self.df["word count"] = self.df["Text"].apply(lambda x:len(str(x).split()))
 
+        antisemitic = (
+            self.df[self.df["Biased"]==1]
+            .sort_values(by="word count",ascending=False).head(3)["Text"].tolist()
+        )
+        non_antisemitic = (
+            self.df[self.df["Biased"]==0]
+            .sort_values(by="word count",ascending=False).head(3)["Text"].tolist()
+        )
+        self.result["longest_3_tweets"] = {
+            "antisemitic":antisemitic,
+            "non_antisemitic": non_antisemitic
+        }
 
     def ten_common_words(self):
         pass
 
     def amount_of_uppercase(self):
+
         pass
 
     def return_dict_result(self):
